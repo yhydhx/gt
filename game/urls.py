6786f9c95f16bb8573d096408a7854579bbb3374
jupiter <yhydhx@gmail.com>
2015-04-01 09:37:30 +0800

    add a index url

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from game import views
 
 urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
     url(r'^index$', views.index, name='index'),
     url(r'^getData$', views.getData, name='getData'),
     url(r'^sInfo$', views.sInfo, name='sInfo'),
