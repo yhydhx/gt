54b52c63eb12a360c46826f8439e4dca88db1327
jupiter <yhydhx@gmail.com>
2015-09-03 10:49:26 +0800

    fix small bug

--- game/views.py
+++ game/views.py
@@ -1,4 +1,4 @@
-d#encoding: utf-8
+#encoding: utf-8
 from django.http import HttpResponse,HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import render,get_object_or_404,RequestContext
